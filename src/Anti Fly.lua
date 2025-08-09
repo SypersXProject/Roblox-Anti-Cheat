14cacd5cb5c4465700aece2d49c53e1cb447f402
SypersXProject <syneyzx@gmail.com>
2025-08-09 23:49:26 +0700

    Update Anti Fly.lua

--- src/Anti Fly.lua
+++ src/Anti Fly.lua
@@ -3,7 +3,7 @@ Anti Fly
 
   How To Use
 local HttpService = game:GetService("HttpService")
-local url = "https://raw.githubusercontent.com/SypersXProject/Roblox-Anti-Cheat/refs/heads/main/src/Anti%20Noclip.lua"
+local url = "https://raw.githubusercontent.com/SypersXProject/Roblox-Anti-Cheat/refs/heads/main/src/Anti%20Fly.lua"
 loadstring(url)
 
 or copy code and paste in game :D
