a772b25dfde1709091961ca841f2c4b7b42aec8e
SypersXProject <syneyzx@gmail.com>
2025-08-10 00:10:02 +0700

    Update Anti Teleport.lua

--- src/Anti Teleport.lua
+++ src/Anti Teleport.lua
@@ -3,7 +3,7 @@ Anti Teleport
 
   How To Use
 local HttpService = game:GetService("HttpService")
-local url = "https://raw.githubusercontent.com/SypersXProject/Roblox-Anti-Cheat/refs/heads/main/src/Anti%20Fly.lua"
+local url = "https://raw.githubusercontent.com/SypersXProject/Roblox-Anti-Cheat/refs/heads/main/src/Anti%20Teleport.lua"
 loadstring(url)
 
 ]]--
