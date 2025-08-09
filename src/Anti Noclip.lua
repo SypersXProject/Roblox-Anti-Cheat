cefc1186e6c71e2db8b29295319db9a5a983217a
SypersXProject <syneyzx@gmail.com>
2025-08-09 23:45:26 +0700

    Update Anti Noclip.lua

--- src/Anti Noclip.lua
+++ src/Anti Noclip.lua
@@ -1,5 +1,12 @@
 --[[
 Anti Noclip
+
+  How To Use
+local HttpService = game:GetService("HttpService")
+local url = "https://raw.githubusercontent.com/SypersXProject/Roblox-Anti-Cheat/refs/heads/main/src/Anti%20Noclip.lua"
+loadstring(url)
+
+
 ]]--
 
 
