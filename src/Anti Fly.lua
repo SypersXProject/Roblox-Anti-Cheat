118b4b0a3efc8e9b469562df8853485a91bf1aa8
SypersXProject <syneyzx@gmail.com>
2025-08-09 23:48:53 +0700

    Create Anti Fly.lua

---
+++ src/Anti Fly.lua
@@ -0,0 +1,43 @@
+--[[
+Anti Fly
+
+  How To Use
+local HttpService = game:GetService("HttpService")
+local url = "https://raw.githubusercontent.com/SypersXProject/Roblox-Anti-Cheat/refs/heads/main/src/Anti%20Noclip.lua"
+loadstring(url)
+
+or copy code and paste in game :D
+]]--
+
+
+
+local a = game:GetService(string.char(82,117,110,83,101,114,118,105,99,101)) 
+local b = game:GetService(string.char(80,108,97,121,101,114,115))
+local c = {}
+local d = 2
+a.Heartbeat:Connect(function(e)
+	for _, f in pairs(b:GetPlayers()) do
+		local g = f.Character
+		if not g then continue end
+		local h = g:FindFirstChild(string.char(72,117,109,97,110,111,105,100))
+		if not h then continue end
+		local i = c[f.Name]
+		if not i then
+			c[f.Name] = {
+				LastCFrameOnGround = g:GetPivot(),
+				TimeAirborne = 0
+			}
+			continue
+		end
+		if h[string.char(70,108,111,111,114,77,97,116,101,114,105,97,108)] == Enum.Material.Air then
+			c[f.Name].TimeAirborne += e
+			if c[f.Name].TimeAirborne > d then
+				c[f.Name].TimeAirborne = 0
+				g:PivotTo(i.LastCFrameOnGround)
+			end
+		else
+			c[f.Name].TimeAirborne = 0
+			c[f.Name].LastCFrameOnGround = g:GetPivot()
+		end
+	end
+end)
